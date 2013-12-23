f815eadfbb158cedb8a36863b0ff748e14721326
alexanderkmd <alexanderk.md@gmail.com>
2013-12-23 19:15:22 +0400

    продолжение пута объектов
    примитив

--- dicomUploader/amazon.py
+++ dicomUploader/amazon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from boto.s3.connection import S3Connection
+from boto.s3.bucket import Key
 import config
 
 amazon = config.get_config_section("Amazon")
@@ -10,3 +11,11 @@ bucket = s3conn.get_bucket(amazon['bucket'])
 
 for item in bucket.list():
     print item.name
+
+
+def put_item_to_bucket(filepath, name, storepath):
+    key = Key(bucket)
+    key.key = storepath + "/" + name
+    key.set_contents_from_filename(filepath)
+
+    return 0
